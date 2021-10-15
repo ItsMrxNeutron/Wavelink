d2d8b42c524217355e4b90b8e88b609de5a76cbc
ItsMrxNeutron <46882716+ItsMrxNeutron@users.noreply.github.com>
2021-10-15 14:17:59 +0700

    Added filters support

--- wavelink/player.py
+++ wavelink/player.py
@@ -324,3 +324,339 @@ class Player(discord.VoiceProtocol):
         await self.node._websocket.send(
             op="seek", guildId=str(self.guild.id), position=position
         )
+
+    async def set_filters(self, equalizerjson: Union[dict, list] = None, karaokejson: dict = None, timescalejson: dict = None, tremolojson: dict = None, vibratojson: dict = None, rotationjson: Union[dict, float] =None, distortionjson: dict = None, channelMixjson: dict = None, lowPassjson: Union[dict, float]= None) -> None:
+        """|coro|
+
+        Set filters, basically equalizer and the other customizations to the song but bundled in one requst
+
+        Parameters
+        ----------
+        equalizerjson: Union[dict, list]
+            Takes in band 0-14, each band has a gain limit from -0.25 to 1.0 (default is 0)
+            Example:{'equalizer': [{'band': 0, 'gain': -0.25}, {'band': 1, 'gain': -0.25}, {'band': 2, 'gain': -0.125},
+                {'band': 3, 'gain': 0.0}, {'band': 4, 'gain': 0.25}, {'band': 5, 'gain': 0.25}, {'band': 6, 'gain': 0.0},
+                {'band': 7, 'gain': -0.25}, {'band': 8, 'gain': -0.25}, {'band': 9, 'gain': 0.0}, {'band': 10, 'gain': 0.0},
+                {'band': 11, 'gain': 0.5}, {'band': 12, 'gain': 0.25}, {'band': 13, 'gain': -0.025}]}
+
+            Example parameter:[(0, -0.25), (1, -0.25), (2, -0.125), (3, 0.0),
+                  (4, 0.25), (5, 0.25), (6, 0.0), (7, -0.25), (8, -0.25),
+                  (9, 0.0), (10, 0.0), (11, 0.5), (12, 0.25), (13, -0.025)]
+
+        karaokejson: dict
+            Uses equalization to eliminate part of a band, usually targeting vocals.
+            Example: "karaoke": {
+                    "level": 1.0,
+                    "monoLevel": 1.0,
+                    "filterBand": 220.0,
+                    "filterWidth": 100.0
+                    },
+
+        timescalejson: dict
+            Changes the speed, pitch, and rate. All default to 1.
+            "timescale": {
+            "speed": 1.0,
+            "pitch": 1.0,
+            "rate": 1.0
+            },
+
+        tremolojson: dict
+            Uses amplification to create a shuddering effect, where the volume quickly oscillates.
+            Example: https://en.wikipedia.org/wiki/File:Fuse_Electronics_Tremolo_MK-III_Quick_Demo.ogv
+            Example: "tremolo": {
+                    "frequency": 2.0, # 0 < x
+                    "depth": 0.5      # 0 < x ≤ 1
+                    },
+
+        vibratojson: dict
+            Similar to tremolo. While tremolo oscillates the volume, vibrato oscillates the pitch.
+            Example: "vibrato": {
+                    "frequency": 2.0, # 0 < x ≤ 14
+                    "depth": 0.5      # 0 < x ≤ 1
+                    },
+
+        rotationjson: Union[dict, float]
+            Rotates the sound around the stereo channels/user headphones aka Audio Panning. It can produce an effect similar to: https://youtu.be/QB9EB8mTKcc (without the reverb)
+            "rotation": {
+            "rotationHz": 0 // The frequency of the audio rotating around the listener in Hz. 0.2 is similar to the example video above.
+            },
+            Or you can pass a float value in bigger than 0
+            Example Parameter: 0.2
+
+        distortionjson: dict
+            Distortion effect. It can generate some pretty unique audio effects.
+            Example: "distortion": {
+                    "sinOffset": 0,
+                    "sinScale": 1,
+                    "cosOffset": 0,
+                    "cosScale": 1,
+                    "tanOffset": 0,
+                    "tanScale": 1,
+                    "offset": 0,
+                    "scale": 1
+                    }
+
+        channelMixjson: dict
+            Mixes both channels (left and right), with a configurable factor on how much each channel affects the other.
+            With the defaults, both channels are kept independent from each other.
+            Setting all factors to 0.5 means both channels get the same audio.
+            Example: "channelMix": {
+                    "leftToLeft": 1.0,
+                    "leftToRight": 0.0,
+                    "rightToLeft": 0.0,
+                    "rightToRight": 1.0,
+                    }
+
+        lowPassjson: Union[dict, float]
+            Higher frequencies get suppressed, while lower frequencies pass through this filter, thus the name low pass.
+            "lowPass": {
+                        "smoothing": 20.0
+                        }
+            Or you can pass a float above 0
+        """
+        if type(equalizerjson) is list:
+            levels = equalizerjson
+            equalizerjson = {"equalizer":[{"band": i[0], "gain": i[1]} for i in levels]}
+        if type(rotationjson) is float:
+            rotationjson = {"rotation": {"rotationHz": rotationjson}}
+        if type(lowPassjson)is float:
+            lowPassjson = {"lowPass":{"smoothing":lowPassjson}}
+
+
+        payload = {
+            "op": "filters",
+            "guildId": str(self.guild.id)
+        }
+
+        if equalizerjson is not None:
+            if type(equalizerjson["equalizer"]) is not list:
+                raise Exception("Deformed equalizerjson")
+            payload.update(equalizerjson)
+        if karaokejson is not None:
+            if type(karaokejson["karaoke"]["level"]) is not float or type(karaokejson["karaoke"]["monoLevel"]) is not float or type(karaokejson["karaoke"]["filterBand"]) is not float or type(karaokejson["karaoke"]["filterWidth"]) is not float:
+                raise Exception("Deformed karaokejson")
+            payload.update(karaokejson)
+        if timescalejson is not None:
+            if type(timescalejson["timescale"]["speed"]) is not float or type(timescalejson["timescale"]["pitch"]) is not float or type(timescalejson["timescale"]["rate"]) is not float:
+                raise Exception("Deformed timescalejson")
+            payload.update(timescalejson)
+        if tremolojson is not None:
+            if type(tremolojson["tremolo"]["frequency"]) is not float or type(tremolojson["tremolo"]["depth"]) is not float:
+                raise Exception("Deformed tremolojson")
+            payload.update(tremolojson)
+        if vibratojson is not None:
+            if type(vibratojson["vibrato"]["frequency"]) is not float or type(vibratojson["vibrato"]["depth"]) is not float:
+                raise Exception("Deformed vibratojson")
+            payload.update(vibratojson)
+        if rotationjson is not None:
+            if type(rotationjson["rotation"]["rotationHz"]) is not float:
+                raise Exception("Deformed rotationjson")
+            payload.update(rotationjson)
+        if distortionjson is not None:
+            if type(distortionjson["distortion"]["sinOffset"]) is not float or type(distortionjson["distortion"]["sinScale"]) is not float or type(distortionjson["distortion"]["cosOffset"]) is not float or type(distortionjson["distortion"]["cosScale"]) is not float or type(distortionjson["distortion"]["tanOffset"]) is not float or type(distortionjson["distortion"]["tanScale"]) is not float or type(distortionjson["distortion"]["offset"]) is not float or type(distortionjson["distortion"]["scale"]) is not float:
+                raise Exception("Deformed distortionjson")
+            payload.update(distortionjson)
+        if channelMixjson is not None:
+            if type(channelMixjson["channelMix"]["leftToLeft"]) is not float or type(channelMixjson["channelMix"]["leftToRight"]) is not float or type(channelMixjson["channelMix"]["rightToLeft"]) is not float or type(channelMixjson["channelMix"]["rightToRight"]) is not float:
+                raise Exception("Deformed channelMix")
+            payload.update(channelMixjson)
+        if lowPassjson is not None:
+            if type(lowPassjson["lowPass"]["smoothing"]) is not float:
+                raise Exception("Deformed lowPassjson")
+            payload.update(lowPassjson)
+
+        await self.node._websocket.send(**payload)
+
+    async def set_equalizer(self, equalizerjson: Union[dict, list]) -> None:
+        """|coro|
+
+        Takes in band 0-14, each band has a gain limit from -0.25 to 1.0 (default is 0)
+
+        Parameters
+        ----------
+        equalizerjson: Union[dict, list]
+            Takes in band 0-14, each band has a gain limit from -0.25 to 1.0 (default is 0)
+            Example:{'equalizer': [{'band': 0, 'gain': -0.25}, {'band': 1, 'gain': -0.25}, {'band': 2, 'gain': -0.125},
+                {'band': 3, 'gain': 0.0}, {'band': 4, 'gain': 0.25}, {'band': 5, 'gain': 0.25}, {'band': 6, 'gain': 0.0},
+                {'band': 7, 'gain': -0.25}, {'band': 8, 'gain': -0.25}, {'band': 9, 'gain': 0.0}, {'band': 10, 'gain': 0.0},
+                {'band': 11, 'gain': 0.5}, {'band': 12, 'gain': 0.25}, {'band': 13, 'gain': -0.025}]}
+
+            Example parameter:[(0, -0.25), (1, -0.25), (2, -0.125), (3, 0.0),
+                  (4, 0.25), (5, 0.25), (6, 0.0), (7, -0.25), (8, -0.25),
+                  (9, 0.0), (10, 0.0), (11, 0.5), (12, 0.25), (13, -0.025)]
+
+        """
+
+
+        await self.set_filters(equalizerjson=equalizerjson)
+        # payload = {
+        #     "op": "filters",
+        #     "guildId": str(self.guild.id),
+        #     "equalizer":[{"band": i[0], "gain": i[1]} for i in levels]
+        # }
+        #
+        # await self.node._websocket.send(**payload)
+
+    async def set_karaoke(self, karaokejson: dict) -> None:
+        """|coro|
+
+        Uses equalization to eliminate part of a band, usually targeting vocals.
+
+        Parameters
+        ----------
+        karaokejson: dict
+            Uses equalization to eliminate part of a band, usually targeting vocals.
+            Example: "karaoke": {
+                    "level": 1.0,
+                    "monoLevel": 1.0,
+                    "filterBand": 220.0,
+                    "filterWidth": 100.0
+                    },
+
+        """
+
+        await self.set_filters(karaokejson=karaokejson)
+
+    async def set_timescale(self, timescalejson: dict) -> None:
+        """|coro|
+
+        Changes the speed, pitch, and rate. All default to 1.
+
+        Parameters
+        ----------
+        timescalejson: dict
+            Changes the speed, pitch, and rate. All default to 1.
+            "timescale": {
+            "speed": 1.0,
+            "pitch": 1.0,
+            "rate": 1.0
+            },
+
+        """
+
+        await self.set_filters(timescalejson=timescalejson)
+
+    async def set_tremolo(self, tremolojson: dict) -> None:
+        """|coro|
+
+        Uses amplification to create a shuddering effect, where the volume quickly oscillates.
+
+        Parameters
+        ----------
+        tremolojson: dict
+            Uses amplification to create a shuddering effect, where the volume quickly oscillates.
+            Example: https://en.wikipedia.org/wiki/File:Fuse_Electronics_Tremolo_MK-III_Quick_Demo.ogv
+            Example: "tremolo": {
+                    "frequency": 2.0, # 0 < x
+                    "depth": 0.5      # 0 < x ≤ 1
+                    },
+
+        """
+
+        await self.set_filters(tremolojson=tremolojson)
+
+    async def set_vibrato(self, vibratojson: dict) -> None:
+        """|coro|
+
+        Similar to tremolo. While tremolo oscillates the volume, vibrato oscillates the pitch.
+
+        Parameters
+        ----------
+        vibratojson: dict
+            Similar to tremolo. While tremolo oscillates the volume, vibrato oscillates the pitch.
+            Example: "vibrato": {
+                    "frequency": 2.0, # 0 < x ≤ 14
+                    "depth": 0.5      # 0 < x ≤ 1
+                    },
+
+        """
+
+        await self.set_filters(vibratojson=vibratojson)
+
+    async def set_rotation(self, rotationjson: Union[dict, float]) -> None:
+        """|coro|
+
+        Rotates the sound around the stereo channels/user headphones aka Audio Panning. It can produce an effect similar to: https://youtu.be/QB9EB8mTKcc (without the reverb)
+
+        Parameters
+        ----------
+        rotationjson: Union[dict, float]
+            Rotates the sound around the stereo channels/user headphones aka Audio Panning. It can produce an effect similar to: https://youtu.be/QB9EB8mTKcc (without the reverb)
+            "rotation": {
+            "rotationHz": 0 // The frequency of the audio rotating around the listener in Hz. 0.2 is similar to the example video above.
+            },
+            Or you can pass a float value in bigger than 0
+            Example Parameter: 0.2
+
+        """
+
+        await self.set_filters(rotationjson=rotationjson)
+
+    async def set_distortion(self, distortionjson: dict) -> None:
+        """|coro|
+
+        Distortion effect. It can generate some pretty unique audio effects.
+
+        Parameters
+        ----------
+        distortionjson: dict
+            Distortion effect. It can generate some pretty unique audio effects.
+            Example: "distortion": {
+                    "sinOffset": 0,
+                    "sinScale": 1,
+                    "cosOffset": 0,
+                    "cosScale": 1,
+                    "tanOffset": 0,
+                    "tanScale": 1,
+                    "offset": 0,
+                    "scale": 1
+                    }
+
+        """
+
+        await self.set_filters(distortionjson=distortionjson)
+
+    async def set_channelMix(self, channelMixjson: dict) -> None:
+        """|coro|
+
+        Mixes both channels (left and right), with a configurable factor on how much each channel affects the other.
+        With the defaults, both channels are kept independent from each other.
+        Setting all factors to 0.5 means both channels get the same audio.
+
+        Parameters
+        ----------
+        channelMixjson: dict
+            Mixes both channels (left and right), with a configurable factor on how much each channel affects the other.
+            With the defaults, both channels are kept independent from each other.
+            Setting all factors to 0.5 means both channels get the same audio.
+            Example: "channelMix": {
+                    "leftToLeft": 1.0,
+                    "leftToRight": 0.0,
+                    "rightToLeft": 0.0,
+                    "rightToRight": 1.0,
+                    }
+
+        """
+
+        await self.set_filters(channelMixjson=channelMixjson)
+
+    async def set_lowPass(self, lowPassjson: Union[dict, float]) -> None:
+        """|coro|
+
+        Higher frequencies get suppressed, while lower frequencies pass through this filter, thus the name low pass.
+
+        Parameters
+        ----------
+        lowPassjson: Union[dict, float]
+            Higher frequencies get suppressed, while lower frequencies pass through this filter, thus the name low pass.
+            "lowPass": {
+                        "smoothing": 20.0
+                        }
+            Or you can pass a float above 0
+
+        """
+
+        await self.set_filters(lowPassjson=lowPassjson)
+
+
+
